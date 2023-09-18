b6498ce4516f0c490b50cdc6739a37501081bbc7
Duc nguyen <ducnguyen150120035555@gmail.com>
2023-09-18 10:35:32 +0700

    init project

---
+++ server.js
@@ -0,0 +1,17 @@
+/** @format */
+
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 8888;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
