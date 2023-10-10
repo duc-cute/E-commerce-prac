27fbcfe9476b4d884ca4a879edb89e479cba6afa
Duc nguyen <ducnguyen150120035555@gmail.com>
2023-10-10 16:19:54 +0700

    #7 paginate,api rating product

--- controller/productController.js
+++ controller/productController.js
@@ -46,6 +46,17 @@ const getProducts = asyncHandler(async (req, res) => {
       const sortBy = req.query.sort.split(",").join(" ");
       query = query.sort(sortBy);
     }
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    }
+    //Paginate
+    const limit = req.query.limit || process.env.LIMIT_PAGE;
+    const page = req.query.page || 1;
+    const skip = (page - 1) * limit;
+    query.skip(skip).limit(limit);
+
     const products = await query;
     const countProducts = await Product.countDocuments(formatQueries);
 
@@ -82,10 +93,50 @@ const deleteProduct = asyncHandler(async (req, res) => {
     deleteProduct: deleteProduct ? deleteProduct : "Can't not delete product",
   });
 });
+const ratings = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const { star, comment, pid } = req.body;
+  if (!star || !pid) throw new Error("Missing inputs");
+  const ratingProduct = await Product.findById(pid);
+  const alreadyProduct = ratingProduct?.ratings?.find(
+    (el) => el.postBy.toString() === _id
+  );
+  if (alreadyProduct) {
+    await Product.updateOne(
+      {
+        ratings: { $elemMatch: alreadyProduct },
+      },
+      {
+        $set: { "ratings.$.star": star, "ratings.$.comment": comment },
+      }
+    );
+  } else {
+    await Product.findByIdAndUpdate(
+      pid,
+      {
+        $push: { ratings: { star, comment, postBy: _id } },
+      },
+      { new: true }
+    );
+  }
+  const updatedProduct = await Product.findByIdAndUpdate(pid);
+  const ratingCount = updatedProduct.ratings.length;
+  const sumRating = updatedProduct.ratings.reduce(
+    (sum, rating) => sum + +rating.star,
+    0
+  );
+  updatedProduct.totalRating = Math.round((sumRating * 10) / ratingCount) / 10;
+  await updatedProduct.save();
+  return res.status(200).json({
+    success: true,
+    updatedProduct,
+  });
+});
 module.exports = {
   createProduct,
   getProduct,
   getProducts,
   updateProduct,
   deleteProduct,
+  ratings,
 };
--- routes/product.js
+++ routes/product.js
@@ -7,6 +7,7 @@ router.post("/", [verifyAccessToken, isAdmin], ctrls.createProduct);
 router.get("/:uid", ctrls.getProduct);
 router.get("/", ctrls.getProducts);
 router.delete("/:uid", [verifyAccessToken, isAdmin], ctrls.deleteProduct);
+router.put("/ratings", verifyAccessToken, ctrls.ratings);
 router.put("/:uid", [verifyAccessToken, isAdmin], ctrls.updateProduct);
 
 module.exports = router;
